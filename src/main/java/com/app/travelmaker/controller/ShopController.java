fd9c42c733e8870572a72b8e82b4afe2926c8a85
developer-cbum <dal.semicolon@gmail.com>
2023-06-22 23:22:00 +0900

    shop 퍼블리싱 작업

---
+++ src/main/java/com/app/travelmaker/controller/ShopController.java
@@ -0,0 +1,20 @@
+package com.app.travelmaker.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/shops/*")
+public class ShopController {
+
+    @GetMapping("list")
+    public void goToList(){;}
+
+    @GetMapping("detail")
+    public void goToDetail(){;}
+
+
+}
