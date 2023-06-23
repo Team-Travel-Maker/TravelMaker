460db6a23ef3a2154c800da0e383f67c18b00753
dodunge <dodunge@gmail.com>
2023-06-23 22:33:41 +0900

    로그인 수정| 계정찾기 추가| 비밀번호 입력, 재설정, 이메일 보내기 부분 추가(전부 js부분은 미작성)

--- src/main/java/com/app/travelmaker/controller/LoginController.java
+++ src/main/java/com/app/travelmaker/controller/LoginController.java
@@ -21,4 +21,28 @@ public class LoginController {
     //계정 찾기
     @GetMapping("find")
     public void goToFind(){;}
+
+    //계정 찾기 성공
+    @GetMapping("found")
+    public void goToFound(){;}
+    
+    //이미 존재하는 계정 확인
+    @GetMapping("exiting")
+    public void goToExiting(){;}
+
+    //비밀번호 입력
+    @GetMapping("password")
+    public void goToPassword(){;}
+
+    //비밀번호 재설정 이메일 전송 확인 화면
+    @GetMapping("password_email")
+    public void goToPasswordEmail(){;}
+
+    //비밀번호 재설정 화면
+    @GetMapping("password_reset")
+    public void goToPasswordReset(){;}
+
+    //비밀번호 재설정 성공 화면
+    @GetMapping("password_reset_ok")
+    public void goToPasswordResetOk(){;}
 }
