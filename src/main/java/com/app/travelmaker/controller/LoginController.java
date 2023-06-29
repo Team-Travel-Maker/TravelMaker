0862154e44e6e9ca9a4338c3a110ed2e8a0d2469
dodunge <dodunge@gmail.com>
2023-06-29 10:14:50 +0900

    font 적용, 패키지 정리

--- src/main/java/com/app/travelmaker/controller/LoginController.java
+++ src/main/java/com/app/travelmaker/controller/LoginController.java
@@ -7,42 +7,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
-@RequestMapping("/login/*")
+@RequestMapping("/accounts/*")
 public class LoginController {
 
     //로그인
-    @GetMapping("login")
+    @GetMapping("login/login")
     public void goToLogin(){;}
 
     //회원 가입
-    @GetMapping("join")
+    @GetMapping("join/join")
     public void goToJoin(){;}
 
     //계정 찾기
-    @GetMapping("find")
+    @GetMapping("account/find")
     public void goToFind(){;}
 
     //계정 찾기 성공
-    @GetMapping("found")
+    @GetMapping("account/found")
     public void goToFound(){;}
     
     //이미 존재하는 계정 확인
-    @GetMapping("exiting")
+    @GetMapping("account/exiting")
     public void goToExiting(){;}
 
     //비밀번호 입력
-    @GetMapping("password")
+    @GetMapping("password/input")
     public void goToPassword(){;}
 
     //비밀번호 재설정 이메일 전송 확인 화면
-    @GetMapping("password_email")
+    @GetMapping("password/email")
     public void goToPasswordEmail(){;}
 
     //비밀번호 재설정 화면
-    @GetMapping("password_reset")
+    @GetMapping("password/reset")
     public void goToPasswordReset(){;}
 
     //비밀번호 재설정 성공 화면
-    @GetMapping("password_reset_ok")
+    @GetMapping("password/reset-ok")
     public void goToPasswordResetOk(){;}
 }
