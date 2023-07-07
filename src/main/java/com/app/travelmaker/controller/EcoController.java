0435302bbb660d0ed577dde8ac2b70435ce941ea
Bjin7916 <jine7916@gmail.com>
2023-07-08 05:24:23 +0900

    에코인증 댓글추가

---
+++ src/main/java/com/app/travelmaker/controller/EcoController.java
@@ -0,0 +1,27 @@
+package com.app.travelmaker.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping(value = "/ecos/*")
+public class EcoController {
+
+//    에코인증 목록
+    @GetMapping("eco-list")
+    public void goToEcoList(){;}
+
+//    에코인증 등록
+    @GetMapping("eco-registration")
+    public void goToRegistration(){;}
+
+//    에코인증 상세
+    @GetMapping("eco-detail")
+    public void goToDetail(){}
+
+
+}
