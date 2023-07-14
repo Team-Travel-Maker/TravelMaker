5fe208c113761d79d92ea3dac253c85d91e0c5e0
dodunge <dodunge@gmail.com>
2023-07-14 15:23:17 +0900

    고객센터 메인화면, 자주묻는 질문 목록/상세 완료

--- src/main/java/com/app/travelmaker/controller/InformationController.java
+++ src/main/java/com/app/travelmaker/controller/InformationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @Slf4j
 @RequestMapping("/informations/*")
-public class informationController {
+public class InformationController {
 
     // 고객 센터 메인 화면
     @GetMapping("main")
@@ -25,5 +25,24 @@ public class informationController {
     // 문의 / 신고 등록 화면
     @GetMapping("inquiry/write")
     public void goToInquiryWrite(){;}
+
+    // 일반회원 자주 묻는 질문 목록
+    @GetMapping("faq/general-list")
+    public void goToFAQGeneralList(){;}
+
+    // 업체회원 자주 묻는 질문 목록
+    @GetMapping("faq/company-list")
+    public void goToFAQCompanyList(){;}
+
+
+
+    //-------일단 정적 페이지 양식만! 나중에 정적으로 여러개 작성할 예정---------------
+    // 일반회원 자주 묻는 질문 상세
+    @GetMapping("faq/general-detail")
+    public void goToFAQGeneralDetail(){;}
+
+    // 일반회원 자주 묻는 질문 상세
+    @GetMapping("faq/company-detail")
+    public void goToFAQCompanyDetail(){;}
 
 }
