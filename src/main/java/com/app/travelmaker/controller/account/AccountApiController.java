24bc7a545991ff31ef7dee22104a2d9b5edc8c75
developer-cbum <dal.semicolon@gmail.com>
2023-07-28 22:20:08 +0900

    컨트롤러

---
+++ src/main/java/com/app/travelmaker/controller/account/AccountApiController.java
@@ -0,0 +1,9 @@
+package com.app.travelmaker.controller.account;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/accounts/*")
+public class AccountApiController {
+}
