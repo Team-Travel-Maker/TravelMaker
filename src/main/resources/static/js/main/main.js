1083a731b1e40370c0a6810372c26fb48b8ad97a
developer-cbum <dal.semicolon@gmail.com>
2023-08-05 00:00:32 +0900

    main 목록 js 끝

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -1,3 +1,13 @@
+/*메인 내용 슬라이드 버튼*/
+const leftBtns = document.querySelectorAll('.list-left-btn');
+const rightBtns = document.querySelectorAll('.list-right-btn');
+const contentBoxes = document.getElementsByClassName('title-list-slick-track');
+
+let list = {};
+let rightFlag = false;
+let checkflag = false;
+
+
 /*슬라이드 배너*/
 const banner = document.querySelector("div.banner-list");
 let count = 1;
@@ -67,3 +77,61 @@ arrows.forEach(arrow => {
         }
     });
 });
+
+
+/*아래 목록 슬라이드 효과*/
+
+/*오른쪽 버튼*/
+rightBtns.forEach((rightBtn,index) => {
+    if(!rightFlag){ list[`right${index+1}`] = 1; if(index==3){rightFlag=true;}}
+    rightBtn.addEventListener("click", function (e) {
+        /*가까운 거 찾기*/
+       const listTrack = e.currentTarget.parentNode.parentNode.parentNode.querySelector('.title-list-slick-track');
+       const leftBtn = e.currentTarget.parentNode.parentNode.parentNode.querySelector('.list-left-btn');
+       const counts = rightBtn.classList[2];
+
+       if(checkflag){
+           list[`right${counts}`]++;
+           checkflag=false;
+       }
+
+       leftBtn.classList.remove('not-allow');
+       leftBtn.disabled=false;
+
+       listTrack.style.transition = "transform 0.5s"
+       listTrack.style.transform = `translate(-${1080 * list[`right${counts}`] }px)`;
+        list[`right${counts}`]++
+        if( list[`right${counts}`] == 6){
+            rightBtn.classList.add('not-allow')
+            rightBtn.disabled=true;
+        }
+    })
+})
+
+/*왼쪽 버튼*/
+leftBtns.forEach((leftBtn,index) => {
+    leftBtn.addEventListener("click", function (e) {
+        /*가까운 거 찾기*/
+        const listTrack = e.currentTarget.parentNode.parentNode.parentNode.querySelector('.title-list-slick-track');
+        const rightBtn = e.currentTarget.parentNode.parentNode.parentNode.querySelector('.list-right-btn');
+        const counts = leftBtn.classList[2];
+
+        if(!checkflag){
+            list[`right${counts}`]--;
+            checkflag=true;
+        }
+
+        list[`right${counts}`]--;
+
+        rightBtn.classList.remove('not-allow');
+        rightBtn.disabled=false;
+
+        listTrack.style.transition = "transform 0.5s"
+        listTrack.style.transform = `translate(-${1080 * (list[`right${counts}`]) }px)`;
+        if( list[`right${counts}`] == 0){
+            leftBtn.classList.add('not-allow')
+            leftBtn.disabled=true;
+        }
+
+    })
+})
