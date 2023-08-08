31550b306b885b0a85b49e67d9109eb1069d3b50
developer-cbum <dal.semicolon@gmail.com>
2023-08-08 23:33:27 +0900

    문의/신고 파일까지 등록 완료

--- src/main/java/com/app/travelmaker/constant/FileType.java
+++ src/main/java/com/app/travelmaker/constant/FileType.java
@@ -12,10 +12,12 @@ public enum FileType {
      * REPRESENTATIVE : 대표 이미지
      * NON_REPRESENTATIVE : 제목 이미지
      * CONTENT_REPRESENTATIVE : 내용 이미지
+     * GENERAL : 일반 파일
      * */
     REPRESENTATIVE("REPRESENTATIVE", "대표 이미지"),
     NON_REPRESENTATIVE("NON_REPRESENTATIVE", "제목 이미지"),
-    CONTENT_REPRESENTATIVE("CONTENT_REPRESENTATIVE", "내용 이미지");
+    CONTENT_REPRESENTATIVE("CONTENT_REPRESENTATIVE", "내용 이미지"),
+    GENERAL("GENERAL", "일반 파일");
 
     private final String code;
     private final String name;
