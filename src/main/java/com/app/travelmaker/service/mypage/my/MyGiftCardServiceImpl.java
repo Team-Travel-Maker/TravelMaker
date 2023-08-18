494893909c12b3cdf0fd1733537a3a5bb92b2f45
dodunge <dodunge@gmail.com>
2023-08-18 18:47:20 +0900

    상품, 업체관련 서비스 세션 적용

--- src/main/java/com/app/travelmaker/service/mypage/my/MyGiftCardServiceImpl.java
+++ src/main/java/com/app/travelmaker/service/mypage/my/MyGiftCardServiceImpl.java
@@ -1,5 +1,6 @@
 package com.app.travelmaker.service.mypage.my;
 
+import com.app.travelmaker.common.CommonSupport;
 import com.app.travelmaker.domain.mypage.my.MyGiftCardDTO;
 import com.app.travelmaker.entity.mebmer.Member;
 import com.app.travelmaker.repository.member.MemberRepository;
@@ -11,15 +12,15 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class MyGiftCardServiceImpl implements MyGiftCardService{
+public class MyGiftCardServiceImpl extends CommonSupport implements MyGiftCardService{
 
     private final MyGiftCardRepository myGiftCardRepository;
     private final MemberRepository memberRepository;
 
     @Override
     public List<MyGiftCardDTO> getGiftCardListByMemberId() {
-        // TODO 세션으로 member객체 받기
-        Member member = memberRepository.findById(32L).orElseThrow(() -> new RuntimeException("해당 멤버 없음"));
+//        Member member = memberRepository.findById(32L).orElseThrow(() -> new RuntimeException("해당 멤버 없음"));
+        Member member = authenticationInfo();
 
         return myGiftCardRepository.getGiftCardListByMemberId(member.getId());
     }
