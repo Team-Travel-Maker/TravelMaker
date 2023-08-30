36bec069402eb7c48223057038b19d20e0e7177e
developer-cbum <dal.semicolon@gmail.com>
2023-08-30 20:14:14 +0900

    리스트 공통 js 제작

--- src/main/resources/static/js/main/list.js
+++ src/main/resources/static/js/main/list.js
@@ -1,60 +1,19 @@
+
 const $lists = $('.title-list-slick-track')
 
-let firstText = `
-
-                `
-
-let MainService = (function () {
-
-
-    function getList(callback, url, index){
-        $.ajax({
-            url: url,
-            type: `get`,
-            async: false,
-            success: function(result){
-                console.log(result)
-                if(callback){
-                    callback(result, index);
-                }
-            }
-        })
-    }
-
-
-    return {getList: getList};
-
-})()
-
-
-MainService.getList(showList, "/api/admins/store" , 0);
-
-
-function showList(results, index) {
-    $lists.eq(index).html('');
-    text="";
-
-    results.content.forEach(result => {
-
-        if(result.storeStatus.code == "PENDING" || result.storeStatus.code == "REJECTED"){return false}
-
-        let fileName;
-        result.storeFiles.forEach(file =>{
-            if(file.fileType.code == "REPRESENTATIVE"){
-                fileName = file.filePath + "/" + file.fileUuid + "_" + file.fileName;
-            }
-        })
-
-        firstText += `
-                <div class="title-list-slick-slide">
+let option = {}
+option.container = $lists.eq(0);
+option.url = "/api/admins/store"
+option.contentText= $(`
+            <div class="title-list-slick-slide">
                     <div class="card-container">
-                        <a href="https://www.wanted.co.kr/company/5927">
-                            <header role="img" style="background-image: url('/api/files/display?fileName=${fileName}');"></header>
+                        <a href="#">
+                            <header role="img" data-name="storeFiles" style="background-image: url('');"></header>
                             <div class="card-body-wrap">
                                 <div class="card-body-flexbox">
                                     <div class="card-body-info">
-                                        <p class="card-body-info-title">${result.storeTitle}</p>
-                                        <p class="card-body-info-category">${result.storeType.name}</p>
+                                        <p class="card-body-info-title" data-class="storeTitle"></p>
+                                        <p class="card-body-info-category" data-class="storeType.name"></p>
                                     </div>
                                 </div>
                                 <button type="button" class="card-body-button">
@@ -65,7 +24,9 @@ function showList(results, index) {
                         </a>
                     </div>
                 </div>
-                    `
-    });
-    $lists.eq(index).html(firstText);
-}
+            `);
+
+
+
+mainService.getList(showList, option);
+
