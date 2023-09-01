dfe0b7fb0062aba198c066950d343e2a4afbe4ba
developer-cbum <dal.semicolon@gmail.com>
2023-09-01 21:07:48 +0900

    메인 리스트 뿌리기

--- src/main/resources/static/js/main/list.js
+++ src/main/resources/static/js/main/list.js
@@ -5,16 +5,22 @@ let option1 = {}
 let option2 = {}
 option1.container = $lists.eq(0);
 option1.url = "/api/admins/store/main"
+/** 반복 돌리고 싶은 틀을 전체를 div로 감싸준다 이유 html으로 가져오기 편함*/
 option1.contentText= $(`
+        <div>
             <div class="title-list-slick-slide">
                     <div class="card-container">
                         <a href="#">
-                            <header role="img" data-name="storeFiles" style="background-image: url('');"></header>
+                            <header 
+                            data-role="img"
+                            data-type="file"
+                            data-key="storeFiles">
+                            </header>
                             <div class="card-body-wrap">
                                 <div class="card-body-flexbox">
                                     <div class="card-body-info">
-                                        <p class="card-body-info-title" data-class="storeTitle"></p>
-                                        <p class="card-body-info-category" data-class="storeType.name"></p>
+                                        <p class="card-body-info-title" data-key="storeTitle"></p>
+                                        <p class="card-body-info-category" data-key="storeType.name"></p>
                                     </div>
                                 </div>
                                 <button type="button" class="card-body-button">
@@ -25,36 +31,19 @@ option1.contentText= $(`
                         </a>
                     </div>
                 </div>
+            </div>
             `);
 option2.container =$lists.eq(1);
 option2.url = "/api/admins/coupon/main"
-/*option2.contentText= $(`
-            <div class="title-list-slick-slide">
-                    <div class="card-container">
-                        <a href="#">
-                            <header role="img" data-name="files" style="background-image: url('');"></header>
-                            <div class="card-body-wrap">
-                                <div class="card-body-flexbox">
-                                    <div class="card-body-info">
-                                        <p class="card-body-info-title" data-class="giftCardTitle"></p>
-                                        <p class="card-body-info-category" data-class="giftCardRegion"></p>
-                                    </div>
-                                </div>
-                                <button type="button" class="card-body-button">
-                                    <span class="card-body-button-label">바로가기</span>
-                                    <span class="card-body-button-interaction"></span>
-                                </button>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            `);*/
-
 option2.contentText= $(`
+         <div>
             <div class="title-list-slick-slide">
                     <div class="card-container">
                         <a href="#">
-                            <header data-role="img" data-type="file" data-key="files" style="background-image: url('');"></header>
+                            <header data-role="img"
+                                    data-type="file" 
+                                    data-key="files">
+                            </header>
                             <div class="card-body-wrap">
                                 <div class="card-body-flexbox">
                                     <div class="card-body-info">
@@ -70,12 +59,13 @@ option2.contentText= $(`
                         </a>
                     </div>
                 </div>
+           <div>   
             `);
 
 
 
 
-/*mainService.getList(showList, option1);*/
+mainService.getList(showList, option1);
 
 mainService.getList(showList, option2);
 
